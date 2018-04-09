b3ca10c1f2638d8c3c5963161017b6dd312d5ffb
thomasxbanks <thomas.banks@wearecube3.com>
2018-04-09 10:52:30 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,25 @@
+var romanNumerals = {
+  1: "I",
+  2: "II",
+  3: "III",
+  4: "IV",
+  5: "V",
+  6: "VI",
+  7: "VII",
+  8: "VIII",
+  9: "IX",
+  10: "X"
+};
+
+var generateRomanNumerals = (value) => {
+  return romanNumerals[value]
+}
+
+var romanNumeralEl = document.getElementById('roman-numeral');
+var form = document.getElementsByTagName('form')[0];
+
+form.addEventListener('submit', function(e){
+  e.preventDefault();
+  var value = form.querySelector('input').value
+  romanNumeralEl.innerText = generateRomanNumerals(value)
+});
