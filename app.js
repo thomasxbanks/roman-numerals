1fc7f823a4aae671fc8e0bf77e02045a0f68423b
thomasxbanks <thomasxbanks@gmail.com>
2018-04-12 19:36:03 +0100

    WIP - some validation stuff

--- app.js
+++ app.js
@@ -1,33 +1,75 @@
-function generateRomanNumerals(value) {
+function generateRomanNumerals(originalValue) {
   var romanNumeralComponents = [
     ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"], // Ones
     ["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"], // Tens
     ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"], // Hundreds
-    ["", "M", "MM", "MMM"] // Thousands. Capped at 3000, as per brief
+    ["", "M", "MM", "MMM"]                                        // Thousands. Capped at 3000, as per brief
   ];
+
+  // Validation, in this specific project, is done at form input level
+  // but the function can be used independently so
+  // validating within the function makes sense
+  if (!originalValue.match(/^[0-9\.]*$/)) {
+    // originalValue is not a number. Here's a random number instead
+    originalValue = ~~(Math.floor(Math.random() * 3999) + 1);
+    // Alternatively, uncomment the below:
+    // return false;
+  }
+  if (originalValue < 1) {
+    // If a lower number gets through, up it to within bounds
+    originalValue = "1";
+    // Alternatively, uncomment the below:
+    // return false;
+  }
+  if (originalValue > 3999) {
+    // If a higher number gets through, drop it to within bounds
+    originalValue = "3999";
+    // Alternatively, uncomment the below:
+    // return false;
+  }
+  if (originalValue % 1 !== 0) {
+    // If a decimal float gets through, round it to the nearest whole number
+    originalValue = Math.round(originalValue);
+    // Alternatively, uncomment the below:
+    // return false;
+  }
+  
+  // Always be needing strings!
+  originalValue = originalValue.toString();
+
   // Reversing the array because there will always be a 1 but not always a thousand
-  var valueArray    = value.split('').reverse();
+  var originalValueArray = originalValue.split("").reverse();
   var newValueArray = [];
-  var valueArrayLength = valueArray.length; // Cache this value so it isn't calculated on every pass
+  var originalValueArrayLength = originalValueArray.length; // Cache this originalValue so it isn't calculated on every pass
 
   // Populate the newValueArray with the Roman Numeral equivalents for each number
-  for (var i = 0; i < valueArrayLength; i++) {
-    newValueArray.push(romanNumeralComponents[i][valueArray[i]]);
+  for (var i = 0; i < originalValueArrayLength; i++) {
+    newValueArray.push(romanNumeralComponents[i][originalValueArray[i]]);
   }
 
   // Then, like Blazin' Squad, we flip reverse it to put the
   // numerals back in the right order
   var newValue    = newValueArray.reverse().join('');
-  return newValue;
+  return { 
+    originalValue: originalValue,
+    newValue: newValue 
+  };
 }
 
 // Show the results to the user on submission of the form
 var romanNumeralEl = document.getElementById('roman-numeral');
-var romanNumeralConvertionFormEl = document.getElementsByTagName('form')[0];
+var romanNumeralConversionFormEl = document.getElementsByTagName('form')[0];
 
-romanNumeralConvertionFormEl.addEventListener('submit', function (e) {
+romanNumeralConversionFormEl.addEventListener('submit', function (e) {
   e.preventDefault();
-  var value = romanNumeralConvertionFormEl.querySelector('input').value;
-  romanNumeralEl.innerHTML = "In Roman Numerals, " + value + ' is <strong aria-label="' + generateRomanNumerals(value).split('').join('-') + '">' + generateRomanNumerals(value) + "</strong>";
+  var originalValue = romanNumeralConversionFormEl.querySelector('input').value;
+
+  romanNumeralEl.innerHTML = 'In Roman Numerals, ' + 
+    generateRomanNumerals(originalValue).originalValue + 
+    ' is <strong aria-label="' + 
+    generateRomanNumerals(originalValue).newValue.split("").join("-") + '">' + 
+    generateRomanNumerals(originalValue).newValue + 
+    '</strong>';
+  
   romanNumeralEl.setAttribute('data-state', '');
 });
