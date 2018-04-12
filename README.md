8e64a213ccdc59dcb800dd7eecdd002655b551c4
Thomas Banks <thomasxbanks@gmail.com>
2018-04-12 23:22:38 +0100

    erroring

--- README.md
+++ README.md
@@ -1,11 +1,40 @@
 # Convert Arabic numbers to Roman Numerals
 
-When provide with an Arabic number between 1 and 3999, this function will return the value in the equivalent Roman Numerals.
+When provide with an Arabic number between 1 and 3999, the `generateRomanNumeral()` function will return the value in the equivalent Roman Numerals.
 
-## Unit-tests
+## Codebase
 
-Tested using Jasmine. Tests pass.
+Written in vanilla JavaScript without libraries to remove any load time.
+I have stayed to ES5 syntax for this as I didn't want to include convoluted compilers to make a single function work on legacy browsers. For the same reason, I have used the CDN version of Jasmine.
 
 ## Design
 
-I have kept everything to a minimum as building a front-end wasn't specifically part of the brief.
+I have kept everything to a minimum as building a front-end wasn't specifically part of the brief. I have tested the front-end on a variety of browsers (see below) as well as ensuring it is keyboard navigable, meets the contrast ratios for WCAG AAA approval, and works acceptably using the _ChromeVox_ browser extension.
+
+## Tests
+
+### Validation
+
+I have assumed that valid input will come only in Arabic numbers, no other character sets have been accounted for.
+
+### Unit tests
+
+Tested using Jasmine. Tests pass.
+
+### Browser test
+
+Tested in
+
+| Operating System | Browser           | Version         | Status |
+| ---------------- | ----------------- | --------------- | ------ |
+| Windows 10       | Chrome            | 65.0.3325.181   | Pass   |
+| Windows 10       | Firefox           | 59.0.2 (64-bit) | Pass   |
+| Windows 10       | Edge              | 41.16299.248.0  | Pass   |
+| Windows 10       | Internet Explorer | 11.309.16299.0  | Pass   |
+| OSX High Sierra  | Chrome            |                 |        |
+| OSX High Sierra  | Firefox           |                 |        |
+| OSX High Sierra  | Safari            |                 |        |
+| Android 8.1      | Chrome            |                 |        |
+| Android 8.1      | Firefox           |                 |        |
+| iOS 9.0          | Chrome            |                 |        |
+| iOS 9.0          | Firefox           |                 |        |
--- app.js
+++ app.js
@@ -1,4 +1,29 @@
 function generateRomanNumerals(originalValue) {
+  // Validation, in this specific project, could be done at form input level
+  // but the function can be used independently so
+  // validating within the function makes sense
+  if (!originalValue.match(/^[0-9\.]*$/)) {
+    // originalValue is not a number.
+    return false;
+  }
+  if (originalValue < 1) {
+    // originalValue is too low.
+    return false;
+  }
+  if (originalValue > 3999) {
+    // originalValue is too high.
+    return false;
+  }
+  if (originalValue % 1 !== 0) {
+    // originalValue is not a whole number.
+    return false;
+  }
+  
+  // Always be needing strings!
+  originalValue = originalValue.toString();
+
+  // Now we have a valid value to use, list out the parts 
+  // that make up a Roman Numeral
   var romanNumeralComponents = [
     ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"], // Ones
     ["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"], // Tens
@@ -6,70 +31,47 @@ function generateRomanNumerals(originalValue) {
     ["", "M", "MM", "MMM"]                                        // Thousands. Capped at 3000, as per brief
   ];
 
-  // Validation, in this specific project, is done at form input level
-  // but the function can be used independently so
-  // validating within the function makes sense
-  if (!originalValue.match(/^[0-9\.]*$/)) {
-    // originalValue is not a number. Here's a random number instead
-    originalValue = ~~(Math.floor(Math.random() * 3999) + 1);
-    // Alternatively, uncomment the below:
-    // return false;
-  }
-  if (originalValue < 1) {
-    // If a lower number gets through, up it to within bounds
-    originalValue = "1";
-    // Alternatively, uncomment the below:
-    // return false;
-  }
-  if (originalValue > 3999) {
-    // If a higher number gets through, drop it to within bounds
-    originalValue = "3999";
-    // Alternatively, uncomment the below:
-    // return false;
-  }
-  if (originalValue % 1 !== 0) {
-    // If a decimal float gets through, round it to the nearest whole number
-    originalValue = Math.round(originalValue);
-    // Alternatively, uncomment the below:
-    // return false;
-  }
-  
-  // Always be needing strings!
-  originalValue = originalValue.toString();
-
-  // Reversing the array because there will always be a 1 but not always a thousand
+  // Reverse the array because there will always be a 1 but not always a thousand
   var originalValueArray = originalValue.split("").reverse();
   var newValueArray = [];
-  var originalValueArrayLength = originalValueArray.length; // Cache this originalValue so it isn't calculated on every pass
+  var originalValueArrayLength = originalValueArray.length; // Cache the length of originalValue so it isn't calculated on every pass
 
   // Populate the newValueArray with the Roman Numeral equivalents for each number
-  for (var i = 0; i < originalValueArrayLength; i++) {
-    newValueArray.push(romanNumeralComponents[i][originalValueArray[i]]);
+  for (var index = 0; index < originalValueArrayLength; index++) {
+    var relevantNumeral = romanNumeralComponents[index][originalValueArray[index]];
+    newValueArray.push(relevantNumeral);
   }
 
   // Then, like Blazin' Squad, we flip reverse it to put the
   // numerals back in the right order
   var newValue    = newValueArray.reverse().join('');
-  return { 
-    originalValue: originalValue,
-    newValue: newValue 
-  };
+  return newValue;
 }
 
 // Show the results to the user on submission of the form
+function generateMessage(originalValue, newValue) {
+  if (newValue === false) {
+    return '<strong>"' + 
+            originalValue + 
+            '"</strong> is not a valid input for this test. Please try again.' +
+            '<br>Valid values are whole numbers between 1 and 3999.';
+  } else {
+    return 'In Roman Numerals, ' +
+      originalValue +
+      ' is <strong aria-label="' +
+      newValue.split("").join("-") +
+      '">' +
+      newValue +
+      '</strong>';
+  }
+}
+
 var romanNumeralEl = document.getElementById('roman-numeral');
 var romanNumeralConversionFormEl = document.getElementsByTagName('form')[0];
 
 romanNumeralConversionFormEl.addEventListener('submit', function (e) {
   e.preventDefault();
   var originalValue = romanNumeralConversionFormEl.querySelector('input').value;
-
-  romanNumeralEl.innerHTML = 'In Roman Numerals, ' + 
-    generateRomanNumerals(originalValue).originalValue + 
-    ' is <strong aria-label="' + 
-    generateRomanNumerals(originalValue).newValue.split("").join("-") + '">' + 
-    generateRomanNumerals(originalValue).newValue + 
-    '</strong>';
-  
+  romanNumeralEl.innerHTML = generateMessage(originalValue, generateRomanNumerals(originalValue));
   romanNumeralEl.setAttribute('data-state', '');
 });
